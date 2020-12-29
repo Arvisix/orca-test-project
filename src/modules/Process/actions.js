64c9b939155d9617c2bc50faef86144ca253ceb0
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 20:46:19 +0100

    Updated process actions

--- src/modules/Process/actions.js
+++ src/modules/Process/actions.js
@@ -1,12 +1,18 @@
-export const PROCESS_LINK_BUTTON_CLICKED = 'PROCESS_LINK_BUTTON_CLICKED';
 export const DELETE_PROCESS_BUTTON_CLICKED = 'DELETE_PROCESS_BUTTON_CLICKED';
-
-export const processLinkButtonClicked = id => ({
-    type: PROCESS_LINK_BUTTON_CLICKED,
-    payload: id,
-});
+export const PROCESSES_PAGE_LOADED = 'PROCESSES_PAGE_LOADED';
+export const ADD_NEW_PROCESS = 'ADD_NEW_PROCESS';
+export const ADD_NEW_PROCESS_BUTTON_CLICKED = 'ADD_NEW_PROCESS_BUTTON_CLICKED';
+export const SET_PROCESSES = 'SET_PROCESSES';
 
 export const deleteProcessButtonClicked = id => ({
     type: DELETE_PROCESS_BUTTON_CLICKED,
     payload: id,
+});
+
+export const processesPageLoaded = () => ({
+    type: PROCESSES_PAGE_LOADED,
+});
+
+export const addNewProcessButtonClicked = () => ({
+    type: ADD_NEW_PROCESS_BUTTON_CLICKED,
 });
