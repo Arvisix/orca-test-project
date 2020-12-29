0b7c9925285b58aa9690001301e763a500f56645
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 20:45:19 +0100

    Updated ProcessPage component

--- src/modules/Process/ProcessPage.js
+++ src/modules/Process/ProcessPage.js
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { AppWrapper, AppHeader } from "./styles";
 
 import ProcessesTable from "./components/ProcessesTable";
 import NewProcessButton from "./components/NewProcessButton";
+import { processesPageLoaded } from "./actions";
 
 const ProcessPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(processesPageLoaded());
+  }, []);
+
   return (
     <AppWrapper>
       <AppHeader>
