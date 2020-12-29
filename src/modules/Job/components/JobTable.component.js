87935abe29c2e3a8dc426c4aca57ccf09382b124
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 19:43:16 +0300

    Moved ColumnFilter component

--- src/modules/Job/components/JobTable.component.js
+++ src/modules/Job/components/JobTable.component.js
@@ -1,22 +1,7 @@
 import React from "react";
-
 import { useTable, useFilters } from "react-table";
 
-function DefaultColumnFilter({
-  column: { filterValue, preFilteredRows, setFilter },
-}) {
-  const count = preFilteredRows.length
-
-  return (
-    <input
-      value={filterValue || ''}
-      onChange={e => {
-        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
-      }}
-      placeholder={`Search ${count} records...`}
-    />
-  )
-}
+import ColumnFilter from '../../../components/Table/ColumnFilter'
 
 const JobTable = () => {
   const columns = React.useMemo(
@@ -34,7 +19,7 @@ const JobTable = () => {
       {
         Header: "Name",
         accessor: "name",
-        Filter: DefaultColumnFilter
+        Filter: ColumnFilter
       },
       {
         Header: "Status",
