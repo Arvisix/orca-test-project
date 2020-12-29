97cf7dbdf0468d80f9fb3945cc766c71b683c789
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 15:40:14 +0300

    Moved App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import App from './modules/App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './configureStore';
+import history from './utils/history';
+
+const store = configureStore({}, history);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
