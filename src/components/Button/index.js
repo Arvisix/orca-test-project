873e1a2eb3130dd8dbf8c1e7c2caa07c97f10622
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 20:26:19 +0100

    Updated Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,19 @@
-const Button = props => {
-    return (
-        <button {...props}>{props.children}</button>
-    )
-}
+import styled from "styled-components";
 
-export default Button
+const Button = (props) => {
+  return <button {...props}>{props.children}</button>;
+};
+
+const StyledButton = styled(Button)`
+  padding: 10px;
+  background-color: transparent;
+  border-color: yellow;
+  border-radius: 5px;
+  color: white;
+  &:hover {
+    cursor: pointer;
+    border-color: red;
+  }
+`;
+
+export default StyledButton;
