493004c9ae11d7d02487af8352e59506369edbe8
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 20:42:19 +0100

    Updated process saga

--- src/modules/Process/saga.js
+++ src/modules/Process/saga.js
@@ -1,18 +1,72 @@
-import { put, takeLatest } from 'redux-saga/effects';
-import { push } from 'connected-react-router'
+import { put, takeLatest, take } from "redux-saga/effects";
+import { eventChannel } from "redux-saga";
+import { push } from "connected-react-router";
+import { v4 as uuid } from "uuid";
+import faker from "faker";
 
-import { PROCESS_LINK_BUTTON_CLICKED, DELETE_PROCESS_BUTTON_CLICKED } from './actions';
-import { processPath } from './constants'
+import firebase from "../../firebase";
+import {
+  DELETE_PROCESS_BUTTON_CLICKED,
+  PROCESSES_PAGE_LOADED,
+  ADD_NEW_PROCESS,
+  SET_PROCESSES,
+  ADD_NEW_PROCESS_BUTTON_CLICKED,
+} from "./actions";
 
-export function* navigateToProcessPage(action) {
-    yield put(push(processPath(action.payload)))
+const ref = firebase.firestore().collection("processes");
+
+function* deleteProcess(action) {
+  yield ref.doc(action.payload).delete();
 }
 
-export function* deleteProcess(action) {
-    yield put(push(processPath(action.payload)))
+function* syncProcesses() {
+  const channel = eventChannel((emit) => ref.onSnapshot(emit));
+
+  try {
+    while (true) {
+      const data = yield take(channel);
+      const processes = data.docs.map((doc) => doc.data());
+
+      yield put({ type: SET_PROCESSES, payload: processes });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function* addProcess({ payload }) {
+  yield ref.doc(payload.id).set(payload);
+}
+
+function* randomizeProcess() {
+  const id = uuid();
+  const name = faker.lorem.word();
+  const startTime = Date.now();
+  const jobsCount = Math.floor(Math.random() * 10) + 1;
+  const arrayOfJobStatuses = ["success", "error", "running"];
+  const jobs = Array.from(Array(jobsCount)).map((job) => ({
+    id: uuid(),
+    name: faker.lorem.word(),
+    status:
+      arrayOfJobStatuses[Math.floor(Math.random() * arrayOfJobStatuses.length)],
+    processId: id,
+  }));
+
+  yield put({
+    type: ADD_NEW_PROCESS,
+    payload: {
+      id,
+      jobsCount,
+      name,
+      startTime,
+      jobs,
+    },
+  });
 }
 
 export default function* processMiddleware() {
-    yield takeLatest(PROCESS_LINK_BUTTON_CLICKED, navigateToProcessPage);
-    yield takeLatest(DELETE_PROCESS_BUTTON_CLICKED, navigateToProcessPage);
+  yield takeLatest(DELETE_PROCESS_BUTTON_CLICKED, deleteProcess);
+  yield takeLatest(ADD_NEW_PROCESS_BUTTON_CLICKED, randomizeProcess);
+  yield takeLatest(PROCESSES_PAGE_LOADED, syncProcesses);
+  yield takeLatest(ADD_NEW_PROCESS, addProcess);
 }
