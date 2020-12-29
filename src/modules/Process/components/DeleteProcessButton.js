b95a80569694b58ca07058daa49aa6f7af86866a
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 20:37:19 +0100

    Updated DeleteProcessButton component

--- src/modules/Process/components/DeleteProcessButton.js
+++ src/modules/Process/components/DeleteProcessButton.js
@@ -1,12 +1,13 @@
 import { useDispatch } from 'react-redux';
 
+import Button from '../../../components/Button'
 import { deleteProcessButtonClicked } from '../actions';
 
 const DeleteProcessButton = ({ row }) => {
   const dispatch = useDispatch();
   const onClick = () => dispatch(deleteProcessButtonClicked(row.original.id))
 
-  return <button onClick={onClick}>🗑️</button>;
+  return <Button onClick={onClick}>🗑️</Button>;
 };
 
 export default DeleteProcessButton;
