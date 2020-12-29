f4a33bda251cef8bb0325403d085e7804a56fe95
Maxim Moroz <maxm.morz@gmail.com>
2020-12-29 20:40:19 +0100

    Added Link component

---
+++ src/components/Link/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Link = props => {
+    return (
+        <a {...props}>{props.children}</a>
+    )
+}
+
+const StyledLink = styled(Link)`
+    color: yellow;
+`;
+
+export default StyledLink
